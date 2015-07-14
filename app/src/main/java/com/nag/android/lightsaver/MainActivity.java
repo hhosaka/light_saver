497d3cd8094735206f1fb7849a0a593c840d14f2
hhosaka <hhosaka183@gmail.com>
2015-07-14 22:50:11 +0900

    release 1.0.0

--- app/src/main/java/com/nag/android/lightsaver/MainActivity.java
+++ app/src/main/java/com/nag/android/lightsaver/MainActivity.java
@@ -5,48 +5,43 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
 
 
 public class MainActivity extends Activity {
     private final String PARAM_BRIGHTNESS = "brightness";
-    private final String PREF_BRIGHTNESS = "pref_brightness";
     private final int MAX_BRIGHTNESS = 255;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int brightness = getIntent().getIntExtra(PARAM_BRIGHTNESS, -1);
-        if(brightness==-1) {
+        int brightness;
+        if(getIntent().hasExtra(PARAM_BRIGHTNESS)) {
+            brightness = getIntent().getIntExtra(PARAM_BRIGHTNESS, -1);
+        }else{
             brightness = Integer.valueOf(Settings.System.getString(this.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS));
-            if(brightness!=MAX_BRIGHTNESS) {
+            if(brightness<MAX_BRIGHTNESS) {
                 setBroadcastReceiver(brightness);
                 brightness = MAX_BRIGHTNESS;
             }
         }
+        Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, brightness);
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.screenBrightness = (float)brightness / MAX_BRIGHTNESS;
+        getWindow().setAttributes(lp);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 finish();
             }
         }, 0);
-        Settings.System.putInt(getContentResolver(),
-                Settings.System.SCREEN_BRIGHTNESS, brightness);
-        WindowManager.LayoutParams lp = getWindow().getAttributes();
-        lp.screenBrightness = Float.valueOf(Settings.System.getString(this.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS))/256;
-        getWindow().setAttributes(lp);
     }
 
     void setBroadcastReceiver(final int brightness) {
-        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
         final BroadcastReceiver receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -56,6 +51,7 @@ public class MainActivity extends Activity {
                 startActivity(i);
             }
         };
-        getApplicationContext().registerReceiver(receiver, filter);
+        getApplicationContext().registerReceiver(receiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+        getApplicationContext().registerReceiver(receiver, new IntentFilter(Intent.ACTION_SHUTDOWN));
     }
 }
